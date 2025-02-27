67050e25af8df564da060abe9aee2d0409353463
Rian122145 <rian.grodrigues@gmail.com>
2025-02-27 19:59:51 -0300

    Commit 3: Adição de nome a saudação

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
-def saudacao():
-    print("Olá, seja bem-vindo ao sistema!")
+def saudacao(nome):
+    print(f"Olá, {nome}! Seja bem-vindo ao sistema!")
 
-print("Versão 2: Adicionando funcionalidade de saudação")
-saudacao()
+print("Versão 3: Adição de nome a saudação")
+
+nome = input("Qual seu nome? ")
+saudacao(nome)
